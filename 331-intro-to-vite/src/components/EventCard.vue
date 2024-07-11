784fafb21ce4a31eca0fccab863d9f3eec86ec96
matchima <mkhamyod@gmail.com>
2024-07-12 06:56:41 +0700

    Q 4.6

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,11 +1,28 @@
+
 <script setup lang="ts">
+
+import {ref} from 'vue'
   // defineProps<{
   //     msg:  string
   // }>
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-class"></div>
 </template>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover{
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
+
 
 
